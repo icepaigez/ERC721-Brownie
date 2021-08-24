c24bac1a28a2d3916c3075a9ae9807ea6db11658
tunde <icepaigez@gmail.com>
2021-08-24 13:47:35 +0100

    updated the function to create a new color nft

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ERC721-Brownie
+>This is a repo that uses eth-brownie to create color NFTs and deployed to the Kovan Network
+ 
+## General Info
+The tokens can be accessed on the Kovan network at the following addresses:  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ColorCollectible - '0xa925A5cB9b1d27410BD524dc36ECC48815E1bB71'  
+
+
+## Inspiration
+Inspired by [Patrick Collins](https://www.youtube.com/watch?v=p36tXHX1JD8)
+
+## Contact
+Created by [@shocotee](https://twitter.com/shocotee) - feel free to contact me!
--- scripts/create_color.py
+++ scripts/create_color.py
@@ -2,7 +2,7 @@ from brownie import ColorCollectible, accounts, config
 from scripts.helper_script import get_color
 import time
 
-STATIC_SEED = 123
+STATIC_SEED = 124
 
 def main():
 	dev = accounts.add(config["wallets"]["from_key"])
@@ -13,5 +13,5 @@ def main():
 	print (transaction)
 	requestId = transaction.events["requestedColor"]["randomId"]
 	color_id = palette_instance.requestIdToColorId(requestId)
-	color = get_color(palette_instance.colorIdToColor(colorId)) 
-	print('Chosen color of {} is {}'.format(color_id, color))
+	color = get_color(palette_instance.colorIdToColor(color_id)) 
+	print('Chosen color of color_id {} is {}'.format(color_id, color))
